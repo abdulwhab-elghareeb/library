bc4c06f73e45905d7d409ca7853f6791e7dc6221
abdulwhab-elghareeb <abdulwhabelghareeb16@gmail.com>
2026-09-07 11:30:24 +0300

    Fix duplicate displayed book issue

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 
-const dialogSubmitBtn = document.querySelector("#dialog-btn")
-
 const library = [];
 
-function Book(title, author, pagesNumber, id, isRead ){
+function Book(title, author, pagesNumber, isRead, id ){
     this.title = title;
     this.author = author;
     this.pagesNumber = pagesNumber;
@@ -21,38 +19,47 @@ function addBookToLibrary(bookTitle, bookAuthor, bookPagesNumber, bookReadingSta
 
 function displayBooks(arrayOfBooks){
     const allCardsContainer = document.querySelector(".book-cards-container");
-    arrayOfBooks.forEach(book => { // looping through all the books
-        const card = document.createElement("div"); // creating the div container for the card
-        card.classList.add("card") ; // adding classes to apply styling
-        card.setAttribute("id" , book.id)
 
-        const title = document.createElement("h1") ; 
-    
-        const authorSpan = document.createElement("span"); 
-        const author = document.createElement("div");
-        author.classList.add("author"); 
-    
-        const pagesSpan = document.createElement("span");
-        const pagesNumber = document.createElement("div");
-        pagesNumber.classList.add("pages-number");
-    
-        const deleteBtn = document.createElement("button");
-        deleteBtn.textContent = "delete me"
-    
-        card.append(title, authorSpan, author, pagesSpan, pagesNumber, deleteBtn); // appending all the elements made to the card to display it later
+    arrayOfBooks.forEach(book => { // looping through all the book/s
 
-        title.textContent = book.title;
-        author.textContent = book.author;
-        pagesNumber.textContent = book.pagesNumber;
+        if(!(document.getElementById(book.id))){// making sure that the book doesn't exist on the dom, avoiding duplicate books
 
-        // spans are the same for each book
-        authorSpan.textContent = "Author:";
-        pagesSpan.textContent = "Pages:";
+            const card = document.createElement("div"); // creating the div container for the card
+            card.classList.add("card") ; // adding classes to apply styling
+            card.setAttribute("id" , book.id) // adding id to each card to mark it 
+            
+            const title = document.createElement("h1") ; 
+            
+            const authorSpan = document.createElement("span"); 
+            const author = document.createElement("div");
+            author.classList.add("author"); 
 
-        allCardsContainer.appendChild(card); // finally, appending the card to the card container
+            const pagesSpan = document.createElement("span");
+            const pagesNumber = document.createElement("div");
+            pagesNumber.classList.add("pages-number");
 
+            const deleteBtn = document.createElement("button");
+            deleteBtn.classList.add("delete-btn")
+            deleteBtn.textContent = "delete me"
+
+
+            card.append(title, authorSpan, author, pagesSpan, pagesNumber, deleteBtn); // appending all the elements made to the card to display it later
+
+            title.textContent = book.title;
+            author.textContent = book.author;
+            pagesNumber.textContent = book.pagesNumber;
+
+            // the same for each book
+            authorSpan.textContent = "Author:";
+            pagesSpan.textContent = "Pages:";
+
+            allCardsContainer.appendChild(card); // finally, appending the card to the card container
+        
+        }
     });
 }
+
+const dialogSubmitBtn = document.querySelector("#dialog-btn")
 
 dialogSubmitBtn.addEventListener("click", (event) =>{
     const dialog = document.querySelector("dialog")
@@ -64,7 +71,7 @@ dialogSubmitBtn.addEventListener("click", (event) =>{
     const bookStatueValue = document.querySelector("input[name='radioStatue']:checked");// getting the checked radio button
 
     addBookToLibrary(bookTitleValue, bookAuthorValue, numberOfPagesValue, bookStatueValue);
-    (document.querySelector("form")).reset()
+    (document.querySelector("form")).reset(); // resetting the form
     displayBooks(library);
-    dialog.close()
+    dialog.close();
 })
